7da22b62f312715ed5551ca9c2ea4b74a90d7f0f
Aditya Kumar <adii.utsav@gmail.com>
2026-03-10 15:26:32 +0530

    first commit

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+
+public class CalculatorTest {
+	
+	@Test
+	public void testAddition() {
+		Calculator calc = new Calculator();
+		int result = calc.add(2, 3);
+		System.out.println("result " + result);
+		assertEquals(7,result);
+	}
+}
